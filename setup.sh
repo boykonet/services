5ce36b62716f4cc057ecd90332fff23f7a77e3de
gkarina <gkarina@student.21-school.ru>
2020-11-12 20:14:40 +0300

    12.11.2020-1

--- setup.sh
+++ setup.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
-#minikube start --vm-driver=virtualbox
+minikube delete
+
+minikube start --vm-driver=virtualbox
+
 minikube addons enable metallb
+
 eval $(minikube docker-env)
 
-docker build -t nginx-image -f ./srcs/nginx_service/Dockerfile ./srcs/nginx_service/
-#docker build -t phpmyadmin_image -f ./srcs/nginx_service/Dockerfile ./srcs/phpmyadmin_service/
-#docker build -t wordpress_image -f ./srcs/nginx_service/Dockerfile ./srcs/wordpress_service/
-#docker build -t grafana_image -f ./srcs/nginx_service/Dockerfile ./srcs/grafana_service/
-#docker build -t influxdb_image -f ./srcs/nginx_service/Dockerfile ./srcs/influxdb_service/
-#docker build -t ftps_image -f ./srcs/nginx_service/Dockerfile ./srcs/ftps_service/
-#docker build -t mysql_image -f ./srcs/nginx_service/Dockerfile ./srcs/mysql_service/
-#docker build -t telegraf_image -f ./srcs/nginx_service/Dockerfile ./srcs/telegraf_service/
-
-kubectl apply -f ./srcs/nginx_service/nginx.yaml
 kubectl apply -f ./configmap.yaml
+
+
+# nginx
+docker build -t nginx-image -f ./srcs/nginx_service/Dockerfile ./srcs/nginx_service/
+kubectl apply -f ./srcs/nginx_service/nginx.yaml
+
+# phpmyadmin
+docker build -t phpmyadmin_image -f ./srcs/phpmyadmin_service/Dockerfile_php ./srcs/phpmyadmin_service/
+kubectl apply -f ./srcs/phpmyadmin_service/php.yaml
+
+# wp
+docker build -t wordpress_image -f ./srcs/wordpress_service/Dockerfile_wp ./srcs/wordpress_service/
+kubectl apply -f ./srcs/wordpress_service/wp.yaml
+
+# grafana
+docker build -t grafana_image -f ./srcs/grafana_service/Dockerfile_grafana ./srcs/grafana_service/
+kubectl apply -f ./srcs/grafana_service/grafana.yaml
+
+# influx
+docker build -t influx_image -f ./srcs/influxdb_service/Dockerfile_influxdb ./srcs/influxdb_service/
+kubectl apply -f ./srcs/influxdb_service/influxdb.yaml
+
+# ftps
+docker build -t ftps_image -f ./srcs/ftps_service/Dockerfile_ftps ./srcs/ftps_service/
+kubectl apply -f ./srcs/ftps_service/ftps.yaml
+
+# mysql
+docker build -t mysql_image -f ./srcs/mysql_service/mysql.yaml ./srcs/mysql_service/
+kubectl apply -f ./srcs/mysql_service/mysql.yaml
+
+# telegraf
+docker build -t telegraf_image -f ./srcs/telegraf_service/Dockerfile_telegraf ./srcs/telegraf_service/
+kubectl apply -f ./srcs/telegraf_service/telegraf.conf
+
